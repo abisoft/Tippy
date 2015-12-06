86f38ca9de18e211807cf32a51d5dc18fd4fb832
mizuho <mizuho@gabesoft-macbook.local>
2015-12-05 17:17:05 -0800

    Initial Commit

---
+++ tipdec5no2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipdec5no2
+//
+//  Created by Mizuho on 12/5/15.
+//  Copyright © 2015 Mizuho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tipdec5no2Tests/tipdec5no2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  tipdec5no2Tests.swift
+//  tipdec5no2Tests
+//
+//  Created by Mizuho on 12/5/15.
+//  Copyright © 2015 Mizuho. All rights reserved.
+//
+
+import XCTest
+@testable import tipdec5no2
+
+class tipdec5no2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
